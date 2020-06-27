3e8c990415c76aba21bba2a20790cd85a975b24d
Simon Bermudez <sabdz55@gmail.com>
2020-06-27 14:07:11 -0400

    remove cronjob

--- app/main.py
+++ app/main.py
@@ -1,31 +1,16 @@
 from flask import Flask, json, jsonify
 import time
-import atexit
-from apscheduler.schedulers.background import BackgroundScheduler
 from .youtube_api import save_data
 from flask_cors import CORS, cross_origin
 
 last_time_updated = time.strftime("%A, %d. %B %Y %I:%M:%S %p")
-
-
-def update_database():
-    last_time_updated = time.strftime("%A, %d. %B %Y %I:%M:%S %p")
-    save_data()
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=update_database, trigger="interval", hours=0.5)
-scheduler.start()
-
-# Shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route("/api.json")
+@app.route("/")
 @cross_origin()
 def api():
     with open('plexus.json', 'r') as file_data:
@@ -33,12 +18,13 @@ def api():
     return jsonify(json_data)
 
 
+@app.route("/sync")
+def sync():
+    last_time_updated = time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+    save_data()
+    return "Synchronization Complete"
+
+
 @app.route("/last")
 def last():
     return last_time_updated
-
-
-@app.route("/sync")
-def sync():
-    save_data()
-    return "Synchronization Complete"
