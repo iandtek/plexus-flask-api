80b5af597cb9044938fd5c72100ad6da28b3cbb5
Simon Bermudez <sabdz55@gmail.com>
2020-07-06 23:06:21 -0400

    added order

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, json, jsonify
+from flask import Flask, json, jsonify, request
 import time
 from .youtube_api import save_data
 from flask_cors import CORS, cross_origin
+import requests
 
 last_time_updated = time.strftime("%A, %d. %B %Y %I:%M:%S %p")
 
@@ -28,3 +29,66 @@ def sync():
 @app.route("/last")
 def last():
     return last_time_updated
+
+
+@app.route("/order", methods=['POST'])
+def order():
+    url = 'http://andtek.ddns.net:3000/api/v1/jobs'
+    raw = json.loads(request.form.get('rawRequest'))
+
+    order = {
+        "user": raw['q3_email3'],
+        "template": {
+            "src": "file:///E:/nexrender/chromatic.aep",
+            "composition": "720p"
+        },
+        "assets": [
+            {
+                "src": raw['logo'][0],
+                "type": "image",
+                "layerName": "logo"
+            },
+            {
+                "type": "data",
+                "layerName": "logo",
+                "property": "Scale",
+                "expression": "h = (thisComp.height/height)*100; w = (thisComp.width/width)*100; if(w > h) {[h,h]} else {[w,w]}"
+            },
+            {
+                "type": "data",
+                "layerName": "page",
+                "property": "Source Text",
+                "value": raw['q17_webpage']
+            }
+        ],
+        "actions": {
+            "postrender": [
+                {
+                    "module": "@nexrender/action-encode",
+                    "preset": "mp4",
+                    "output": "encoded.mp4",
+                    "params": {"-vcodec": "libx264", "-r": 25}
+                },
+                {
+                    "module": "@nexrender/action-copy",
+                    "input":  "encoded.mp4",
+                    "output": "e:/Nexrender/Lookout 2.mp4"
+                },
+                {
+                    "module": "nexrender-action-cloudinary",
+                    "input": "encoded.mp4",
+                    "params": {
+                        "api_key": "545734925218622",
+                        "cloud_name": "dvwbenmc8",
+                        "api_secret": "BLEz7MHdcna5-gdPbIHore07EYM",
+                        "UploadApiOptions": {
+                            "resource_type": "video"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+    print(order)
+    x = requests.post(url, json=order)
+    return x.text
